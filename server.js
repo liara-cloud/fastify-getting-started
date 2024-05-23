8b361085e4f6e8249f8c8adc1be3cc4e5a762457
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-05-23 09:56:47 +0330

    init project

---
+++ server.js
@@ -0,0 +1,23 @@
+const fastify = require('fastify')({ logger: true });
+const path = require('path');
+
+
+fastify.register(require('@fastify/static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/', 
+});
+
+fastify.get('/', (request, reply) => {
+  reply.sendFile('index.html', path.join(__dirname));
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+    fastify.log.info(`Server listening on ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
